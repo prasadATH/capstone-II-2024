4db8ef49da736c1f64da396c02e1f0c3fa5d1ebb
Kande Joel Shameera Kumara <j.kande@student.curtin.edu.au>
2024-07-25 14:57:42 +0530

    test

--- Backend/src/main/java/com/eduexplore/ccp2/service/StudentServiceImpl.java
+++ Backend/src/main/java/com/eduexplore/ccp2/service/StudentServiceImpl.java
@@ -1,4 +1,4 @@
-package com.joelcodes.studentsystem.service;
+package com.eduexplore.ccp2.service;
 
 import com.joelcodes.studentsystem.controller.AuthController;
 import com.joelcodes.studentsystem.model.Student;
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import java.util.List;
 import java.util.Random;
 
@@ -23,13 +22,8 @@ public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
-
-
-
     @Autowired
     private JavaMailSender javaMailSender;
-
-
 
     @Override
     public Traveller saveTraveller(Traveller traveller) {
@@ -39,9 +33,11 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public Traveller loginTraveller(String username, String password) {
         Traveller traveller = studentRepository.findByUsername(username);
-        logger.info("Searched for: {}"+ username+ password);
+        logger.info("Searched for: {}" + username + password);
 
-        //logger.info("Searched for: {}", studentRepository.findByUsername(username).getUsername()+ studentRepository.findByUsername(username).getPassword());
+        // logger.info("Searched for: {}",
+        // studentRepository.findByUsername(username).getUsername()+
+        // studentRepository.findByUsername(username).getPassword());
         if (traveller != null && traveller.getPassword().equals(password)) {
             return traveller;
         }
@@ -49,15 +45,16 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    public String sendOtp(String email,String username) {
+    public String sendOtp(String email, String username) {
         // Verify email input against your database
         Traveller traveller = studentRepository.findByEmail(email);
         Traveller traveller2 = studentRepository.findByUsername(username);
 
-        //if (traveller != null && traveller.getEmail().equals(email))
-        if(traveller != null && traveller2.getEmail().equals(email)){ //This means that the email is equal to the added usernames email so its the same person
+        // if (traveller != null && traveller.getEmail().equals(email))
+        if (traveller != null && traveller2.getEmail().equals(email)) { // This means that the email is equal to the
+                                                                        // added usernames email so its the same person
             String otp = generateOtp(10); // implement this function to generate a random OTP
-            //sendEmail(email, otp);
+            // sendEmail(email, otp);
             traveller2.setOtp(otp);
             studentRepository.save(traveller2);
 
@@ -70,12 +67,9 @@ public class StudentServiceImpl implements StudentService {
 
             return otp;
 
-        }
-        else {
+        } else {
             throw new RuntimeException("Invalid email address.");
         }
-
-
 
         // Generate OTP and send to user's email address
 
@@ -85,13 +79,13 @@ public class StudentServiceImpl implements StudentService {
     public String VerifyOtp(String otp, String username) {
         Traveller traveller = studentRepository.findByUsername(username);
 
-        if(traveller != null && traveller.getOtp().equals(otp)){ //This means that the email is equal to the added usernames email so its the same person
+        if (traveller != null && traveller.getOtp().equals(otp)) { // This means that the email is equal to the added
+                                                                   // usernames email so its the same person
 
-            String message="OTP is correct";
+            String message = "OTP is correct";
             return message;
 
-        }
-        else {
+        } else {
             throw new RuntimeException("Invalid email address.");
         }
     }
@@ -103,11 +97,9 @@ public class StudentServiceImpl implements StudentService {
         studentRepository.save(traveller);
         logger.info("Searched for pw in implementation: {}", password);
 
-
-        String message="password Changed SUCCESSFULLY!!!";
+        String message = "password Changed SUCCESSFULLY!!!";
         return password;
     }
-
 
     private String generateOtp(int length) {
         // Create a Random instance
